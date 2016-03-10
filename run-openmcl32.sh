9ef30ee57eb90e2e2b35142b04333e4fef4f60ff
Shannon Spires <svs@bearlanding.com>
2016-03-09 17:13:58 -0700

    Added some files

---
+++ run-openmcl32.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#OPENMCL=${OPENMCL:-openmcl}
+OPENMCL=ccl32
+OPENMCL_OPT="--batch --no-init"
+
+make clean optimize-files
+${OPENMCL} ${OPENMCL_OPT} --load sysdep/setup-openmcl.lisp --load do-compilation-script.lisp --eval '(ccl:quit)'
+${OPENMCL} ${OPENMCL_OPT} --load sysdep/setup-openmcl.lisp --load do-execute-script.lisp --eval '(ccl:quit)'
